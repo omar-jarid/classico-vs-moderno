a722566edd8eeda3df04692ff6fa1a9bac125ef6
Omar Jarid <omarjarid1993@gmail.com>
2025-06-13 12:03:54 +0200

    Add point 3

--- script.js
+++ script.js
@@ -33,10 +33,13 @@ btn.addEventListener("click", () => {
     const esitoPrimoStudente = primoStudente.voto >= 6 ? "Promosso" : "Bocciato";
     log("Esito del primo studente", `${primoStudente.nome} - Voto: ${primoStudente.voto} - Esito: ${esitoPrimoStudente}`);
 
-    
     // 2. Spread
     const studente1 = { nome: "Giulia Rossi", voto: 7, presente: true };
     const studente2 = { nome: "Marco Bianchi", voto: 5, presente: false };
     const tutti = [...studenti, studente1, studente2];
     log("Classe aggiornata", tutti);
+
+    // 3. Rest
+    const raggruppaNomi = (...nomi) => nomi; 
+    log("Raggruppamento nomi", raggruppaNomi("Mario", "Luigi", "Anna", "Beatrice", "Carlo", "Davide", "Elena", "Federico", "Giorgia", "Lorenzo", "Martina", "Simone"));
 });
